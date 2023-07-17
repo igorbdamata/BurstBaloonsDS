c51d21308d427c70d90cb5d129d232ee410d7770
igorbdamata <igorbdamata@gmail.com>
2023-07-17 14:41:58 -0300

    Backgrounds se sobrepondo

--- BurstBalloonsDS/source/Scenes/TitleScreenScene.cpp
+++ BurstBalloonsDS/source/Scenes/TitleScreenScene.cpp
@@ -12,6 +12,8 @@
 
 #include "BurstBalloonsText0.h";
 #include "BurstBalloonsText1.h";
+#include "BackgroundGameOver.h";
+#include "Background1.h";
 
 TitleScreenScene::TitleScreenScene(OamEngine* mainEngine, OamEngine* subEngine, SceneManager* sceneManager) : Scene(mainEngine, subEngine)
 {
@@ -20,12 +22,14 @@ TitleScreenScene::TitleScreenScene(OamEngine* mainEngine, OamEngine* subEngine, 
 	float pressAnyKeyTextPositionX = SCREEN_WIDTH / 2 - 64 * 2 + 24;
 	float pressAnyKeyTextPositionY = SCREEN_HEIGHT / 2;
 
+
+
 	Entity* pressAnyKeyText[4] = { new Entity(new Vector2(pressAnyKeyTextPositionX , pressAnyKeyTextPositionY), SpriteSize_64x64,64,64),
 		new Entity(new Vector2(pressAnyKeyTextPositionX + 64, pressAnyKeyTextPositionY), SpriteSize_64x64,64,64),
 		new Entity(new Vector2(pressAnyKeyTextPositionX + 64 * 2,pressAnyKeyTextPositionY), SpriteSize_64x64,64,64),
 		new Entity(new Vector2(pressAnyKeyTextPositionX + 64 * 3, pressAnyKeyTextPositionY), SpriteSize_64x64,64,64) };
 
-	
+
 	for (int i = 0; i < 4; i++)
 	{
 		this->pressAnyKeyText[i] = pressAnyKeyText[i];
@@ -55,20 +59,42 @@ TitleScreenScene::TitleScreenScene(OamEngine* mainEngine, OamEngine* subEngine, 
 
 	this->burstBalloonsText[0]->spriteAddress = mainEngine->GetSprite("BurstBalloonsText0");
 	this->burstBalloonsText[1]->spriteAddress = mainEngine->GetSprite("BurstBalloonsText1");
-	
+
 }
 
 void TitleScreenScene::Load()
 {
 	Scene::Load();
+	dmaCopyHalfWords(2,
+					 BackgroundGameOverBitmap,
+					 BG_BMP_RAM(0),
+					 BackgroundGameOverBitmapLen);
+	dmaCopyHalfWords(3,
+					 Background1Bitmap,
+					 BG_BMP_RAM(7),
+					 Background1BitmapLen);
 }
 
 void TitleScreenScene::InputLoop()
 {
 	Scene::InputLoop();
+	int x = 0;
+	int y = 0;
+	//if (keysDown())
+		//sceneManager->ChangeSceneTo("Gameplay");
 
-	if (keysDown())
-		sceneManager->ChangeSceneTo("Gameplay");
+	scanKeys();
+	if (keysHeld() & KEY_LEFT)
+		x = -1;
+	if (keysHeld() & KEY_RIGHT)
+		x = 1;
+	if (keysHeld() & KEY_UP)
+		y = -1;
+	if (keysHeld() & KEY_DOWN)
+		y = 1;
+	bgScroll(2, -x, -y);
+	bgUpdate();
+
 }
 void TitleScreenScene::GameLoop()
 {
@@ -82,4 +108,6 @@ void TitleScreenScene::GameLoop()
 		this->burstBalloonsText[i]->position->y = sin(HardwareManager::GetCurrentMilliseconds() / 930) * 15 + SCREEN_HEIGHT / 2;
 		burstBalloonsText[i]->Render();
 	}
+
+
 }
